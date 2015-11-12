10d9b7a7a090089834ca2db4a0215550443c9a0e
Ben Brittain <ben@brittain.org>
2015-11-12 01:43:11 -0500

    desired seL4 library

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,19 @@
 extern crate sel4_sys;
 use sel4_sys as seL4;
 
+pub fn _shroom_init() {
+    let dma_addr: seL4::types::Word;
+    // seL4::Word low, high;
+    /* Retrieve boot info from seL4 */
+    let boot_info = seL4::getBootInfo();
+
+}
+
 #[no_mangle]
 pub extern fn _sel4_main() {
-//    seL4::InitBootInfo();
+    // start the OS
+    _shroom_init();
+
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
